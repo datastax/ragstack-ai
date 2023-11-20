fcfe825c15702208966291cda1f3effce1342b64
Nicolò Boschi <boschi1997@gmail.com>
2023-11-20 15:00:24 +0100

    fixes

--- dev/run-e2e-tests.sh
+++ dev/run-e2e-tests.sh
@@ -12,16 +12,14 @@ check_env() {
 # astra
 check_env ASTRA_DB_TOKEN
 check_env ASTRA_DB_ENDPOINT
-#export ASTRA_KEYSPACE=ragstacke2e
-#export ASTRA_TABLE_NAME=documents_$(echo $RANDOM | md5sum | head -c 20)
-export ASTRA_KEYSPACE=temp
-export ASTRA_TABLE_NAME=documents
+export ASTRA_KEYSPACE=ragstacke2e
+export ASTRA_TABLE_NAME=documents_$(echo $RANDOM | md5sum | head -c 20)
 
 # open-ai
 check_env OPEN_AI_KEY
 # azure-open-ai
 check_env AZURE_OPEN_AI_KEY
-export AZURE_OPEN_AI_ENDPOINT="https://datastax-openai-dev.openai.azure.com/"
+check_env AZURE_OPEN_AI_ENDPOINT
 export AZURE_OPEN_AI_CHAT_MODEL_DEPLOYMENT="gpt-35-turbo"
 export AZURE_OPEN_AI_EMBEDDINGS_MODEL_DEPLOYMENT="text-embedding-ada-002"
 
