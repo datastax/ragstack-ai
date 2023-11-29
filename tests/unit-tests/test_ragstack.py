cfeefbf7dccaa78e158c750c59f196faa60d8e08
Christophe Bornet <cbornet@hotmail.com>
2023-11-29 14:34:15 +0100

    Add important dependencies to check in unit test

--- tests/unit-tests/test_ragstack.py
+++ tests/unit-tests/test_ragstack.py
@@ -1,3 +1,8 @@
 def test_import():
-    from langchain.vectorstores import Cassandra  # noqa
+    from langchain.vectorstores import AstraDB  # noqa
+    import langchain_core  # noqa
     import langsmith  # noqa
+    import astrapy  # noqa
+    import unstructured  # noqa
+    import openai  # noqa
+    import tiktoken  # noqa
