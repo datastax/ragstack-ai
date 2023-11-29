45823ae3655971c7c7ea69096e3aea92787c16ac
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-11-29 16:29:02 +0100

    Fix

--- ragstack-e2e-tests/e2e_tests/test_astra.py
+++ ragstack-e2e-tests/e2e_tests/test_astra.py
@@ -91,20 +91,26 @@ def test_basic_metadata_filtering(environment):
         collection.find_one(filter={"metadata.chunks": {"$gt": 2}})
         pytest.fail("Should have thrown ValueError")
     except ValueError as e:
+        print("Error:", e)
         if not ("UNSUPPORTED_FILTER_OPERATION" in e.args[0]):
             pytest.fail(
-                "Should have thrown ValueError with UNSUPPORTED_FILTER_OPERATION"
+                f"Should have thrown ValueError with UNSUPPORTED_FILTER_OPERATION but it was {e}"
             )
 
         # This looks very ugly, but it's the only way to get the error message
         # reference ticket on Astrapy https://github.com/datastax/astrapy/issues/126
         errors = json.loads(e.args[0])
-        if len(errors) > 1:
-            error = errors[1]
+        if len(errors) == 1:
+            error = errors[0]
             assert error.get("errorCode") == "UNSUPPORTED_FILTER_OPERATION"
+        elif len(errors) > 1:
+            assert (
+                errors[0].get("errorCode") == "UNSUPPORTED_FILTER_OPERATION"
+                or errors[1].get("errorCode") == "UNSUPPORTED_FILTER_OPERATION"
+            )
         else:
             pytest.fail(
-                "Should have thrown ValueError with UNSUPPORTED_FILTER_OPERATION"
+                f"Should have thrown ValueError with UNSUPPORTED_FILTER_OPERATION but it was {e}"
             )
 
 
