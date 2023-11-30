6b35271ef040aedc28864085d50628c8905195e2
Christophe Bornet <cbornet@hotmail.com>
2023-11-30 11:47:26 +0100

    Reactivate notebook tests (#55)
    Co-authored-by: Enrico Olivelli <enrico.olivelli@datastax.com>

--- examples/notebooks/conftest.py
+++ examples/notebooks/conftest.py
@@ -1,5 +1,5 @@
 import os
 import uuid
 
-os.environ["ASTRA_DB_KEYSPACE"] = "ragstacke2e"
+os.environ["ASTRA_DB_KEYSPACE"] = "default_keyspace"
 os.environ["ASTRA_DB_COLLECTION"] = f"astra_demo_{str(uuid.uuid4()).split('-')[0]}"
