0347c3362dd198f7d1a1fcf10e734c58db9007a2
Christophe Bornet <cbornet@hotmail.com>
2023-12-04 16:42:19 +0100

    Unit tests improvements (#76)

--- libs/ragstack-ai-llamaindex/tests/test_ragstack_llamaindex.py
+++ libs/ragstack-ai-llamaindex/tests/test_ragstack_llamaindex.py
@@ -1,4 +1,5 @@
 def test_import():
+    from ragstack_ai_llamaindex import __version__  # noqa
     from llama_index.vector_stores import AstraDBVectorStore, CassandraVectorStore  # noqa
     import astrapy  # noqa
     import cassio  # noqa
