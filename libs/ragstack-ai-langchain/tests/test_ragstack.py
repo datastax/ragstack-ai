b1cb77a1eefe27286db09d0b3d6ac09c75cd21a3
Christophe Bornet <cbornet@hotmail.com>
2023-12-04 15:50:35 +0100

    Add ragstack-langchain unit tests (#74)

---
+++ libs/ragstack-ai-langchain/tests/test_ragstack.py
@@ -0,0 +1,9 @@
+def test_import():
+    from ragstack_ai_langchain import __version__  # noqa
+    from langchain.vectorstores import AstraDB  # noqa
+    import langchain_core  # noqa
+    import langsmith  # noqa
+    import astrapy  # noqa
+    import unstructured  # noqa
+    import openai  # noqa
+    import tiktoken  # noqa
