90025fdc096509c0afaa347427b0b5f7dcefa90f
Christophe Bornet <cbornet@hotmail.com>
2023-12-04 16:14:40 +0100

    Unit tests improvements

--- tests/unit-tests/test_ragstack.py
+++ tests/unit-tests/test_ragstack.py
@@ -1,8 +1,12 @@
 def test_import():
+    from ragstack_ai_langchain import __version__  # noqa
+    from ragstack_ai_llamaindex import __version__  # noqa
+    from llama_index.vector_stores import AstraDBVectorStore, CassandraVectorStore  # noqa
     from langchain.vectorstores import AstraDB  # noqa
     import langchain_core  # noqa
     import langsmith  # noqa
     import astrapy  # noqa
+    import cassio  # noqa
     import unstructured  # noqa
     import openai  # noqa
     import tiktoken  # noqa
