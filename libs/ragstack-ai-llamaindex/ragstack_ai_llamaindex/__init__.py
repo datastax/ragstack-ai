76314ddcf25e81e4a49ebdf2aecdb3fe9eafdd99
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-12-04 17:01:00 +0100

    LlamaIndex - disable LlamaHub

--- libs/ragstack-ai-llamaindex/ragstack_ai_llamaindex/__init__.py
+++ libs/ragstack-ai-llamaindex/ragstack_ai_llamaindex/__init__.py
@@ -1,8 +1,19 @@
 from importlib import metadata
 
+import llama_index
+
 try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
     __version__ = ""
 del metadata  # optional, avoids polluting the results of dir(__package__)
+
+
+# LlamaHub is disabled while using RAGStack AI, because it would download unknown software from the Internet.
+def disabled_download_loader():
+    raise ImportError(
+        "LlamaHub is disabled while using RAGStack AI, because it would download unknown software from the Internet.")
+
+
+llama_index.download_loader = disabled_download_loader
