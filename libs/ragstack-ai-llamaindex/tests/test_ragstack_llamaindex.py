f292e90bbd76039bb46700955cb7b7a6b0831687
Nicolò Boschi <boschi1997@gmail.com>
2023-12-05 09:18:24 +0100

    Move to single package (#82)

--- libs/ragstack-ai-llamaindex/tests/test_ragstack_llamaindex.py
+++
@@ -1,5 +0,0 @@
-def test_import():
-    from ragstack_ai_llamaindex import __version__  # noqa
-    from llama_index.vector_stores import AstraDBVectorStore, CassandraVectorStore  # noqa
-    import astrapy  # noqa
-    import cassio  # noqa
