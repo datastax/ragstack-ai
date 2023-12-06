a7352d5cfb360d18a34adaf581d474dd1d703197
Nicolò Boschi <boschi1997@gmail.com>
2023-12-06 03:52:47 -0800

    Add security scan (Snyk) (#90)
    * Add security scan (Snyk)
    
    * improve
    
    * improve
    
    * improve
    
    * process
    
    * process
    
    * post
    
    * f

---
+++ scripts/parse-snyk-report.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: parse-snyk-report.py {input_json} {output_file}")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(input_file, 'r') as file:
+    data = json.load(file)
+    vulnerabilities = data.get('vulnerabilities', [])
+
+    with open(output_file, 'w') as output:
+        for vulnerability in vulnerabilities:
+            title = vulnerability.get('title', 'N/A')
+            cvssScore = vulnerability.get('cvssScore', 'N/A')
+            from_packages = ' -> '.join(vulnerability.get('from', []))
+
+            report_line = f"{title} | {from_packages} | {cvssScore}\n"
+            output.write(report_line)
