2975f777217bb2cd16d91b8b3fe7bc943d0faf23
Christophe Bornet <cbornet@hotmail.com>
2023-12-09 17:29:36 +0100

    Refactor lanchain e2e tests (#110)

--- ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_matrix.py
+++ ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_matrix.py
@@ -1,7 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
 import cassio
 import pytest
-from astrapy.db import AstraDB as LibAstraDB
 from e2e_tests.conftest import (
+    AstraRef,
     set_current_test_info,
     get_required_env,
     get_astra_dev_ref,
@@ -20,78 +23,123 @@ from langchain.embeddings import (
 from langchain.embeddings.azure_openai import AzureOpenAIEmbeddings
 from langchain.llms.huggingface_hub import HuggingFaceHub
 from langchain.schema.embeddings import Embeddings
-from langchain.schema.language_model import BaseLanguageModel
 from langchain.schema.vectorstore import VectorStore
 from langchain.vectorstores import AstraDB, Cassandra
 
-VECTOR_ASTRADB_PROD = "astradb-prod"
-VECTOR_ASTRADB_DEV = "astradb-dev"
-VECTOR_CASSANDRA = "cassandra"
+
+class ContextMixin(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @abstractmethod
+    def __enter__(self):
+        ...
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
-def init_vector_db(impl, embedding: Embeddings) -> VectorStore:
-    if impl == VECTOR_ASTRADB_DEV:
-        ref = get_astra_dev_ref()
+class VectorStoreContext(ContextMixin, ABC):
+    def __init__(self, embedding: Embeddings):
+        self.embedding = embedding
 
-        # Ensure collections from previous runs are cleared
-        delete_all_astra_collections(ref)
+    @abstractmethod
+    def __enter__(self) -> VectorStore:
+        ...
 
+
+class AstraCloudVectorStoreContext(VectorStoreContext, ABC):
+    def __init__(self, astra_ref: AstraRef, embedding: Embeddings):
+        super().__init__(embedding)
+        self.astra_ref = astra_ref
+
+    def __enter__(self):
+        delete_all_astra_collections(self.astra_ref)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        delete_astra_collection(self.astra_ref)
+
+
+class AstraDBVectorStoreContext(AstraCloudVectorStoreContext, ABC):
+    def __enter__(self) -> AstraDB:
+        super().__enter__()
         return AstraDB(
-            collection_name=ref.collection,
-            embedding=embedding,
-            token=ref.token,
-            api_endpoint=ref.api_endpoint,
+            collection_name=self.astra_ref.collection,
+            embedding=self.embedding,
+            token=self.astra_ref.token,
+            api_endpoint=self.astra_ref.api_endpoint,
         )
-    elif impl == VECTOR_ASTRADB_PROD:
-        ref = get_astra_prod_ref()
 
-        # Ensure collections from previous runs are cleared
-        delete_all_astra_collections(ref)
 
-        return AstraDB(
-            collection_name=ref.collection,
-            embedding=embedding,
-            token=ref.token,
-            api_endpoint=ref.api_endpoint,
-        )
-    elif impl == VECTOR_CASSANDRA:
-        ref = get_astra_prod_ref()
+class DevAstraDBVectorStoreContext(AstraDBVectorStoreContext):
+    name = "astradb-dev"
 
-        # Ensure collections from previous runs are cleared
-        delete_all_astra_collections(ref)
+    def __init__(self, embedding: Embeddings):
+        super().__init__(get_astra_dev_ref(), embedding)
 
-        cassio.init(token=ref.token, database_id=ref.id)
+
+class ProdAstraDBVectorStoreContext(AstraDBVectorStoreContext):
+    name = "astradb-prod"
+
+    def __init__(self, embedding: Embeddings):
+        super().__init__(get_astra_prod_ref(), embedding)
+
+
+class CassandraVectorStoreContext(AstraCloudVectorStoreContext):
+    name = "cassandra"
+
+    def __init__(self, embedding: Embeddings):
+        super().__init__(get_astra_prod_ref(), embedding)
+
+    def __enter__(self) -> Cassandra:
+        super().__enter__()
+        cassio.init(token=self.astra_ref.token, database_id=self.astra_ref.id)
         return Cassandra(
-            embedding=embedding,
+            embedding=self.embedding,
             session=None,
             keyspace="default_keyspace",
-            table_name=ref.collection,
+            table_name=self.astra_ref.collection,
         )
-    else:
-        raise Exception("Unknown vector db implementation: " + impl)
 
 
-def astra_delete_collection(
-    api_endpoint: str, token: str, collection_name: str
-) -> None:
-    raw_client = LibAstraDB(api_endpoint=api_endpoint, token=token)
-    raw_client.delete_collection(collection_name)
+class OpenAILLMContext(ContextMixin):
+    name = "openai"
+
+    def __enter__(self) -> ChatOpenAI:
+        return ChatOpenAI(
+            openai_api_key=get_required_env("OPEN_AI_KEY"),
+            model="gpt-3.5-turbo-16k",
+            streaming=True,
+            temperature=0,
+        )
 
 
-def close_vector_db(impl: str, vector_store: VectorStore):
-    if impl == VECTOR_ASTRADB_DEV:
-        delete_astra_collection(get_astra_dev_ref())
-    elif impl == VECTOR_ASTRADB_PROD or impl == VECTOR_CASSANDRA:
-        delete_astra_collection(get_astra_prod_ref())
-    else:
-        raise Exception("Unknown vector db implementation: " + impl)
+class OpenAIEmbeddingsContext(ContextMixin):
+    name = "openai"
+
+    def __enter__(self) -> OpenAIEmbeddings:
+        return OpenAIEmbeddings(openai_api_key=get_required_env("OPEN_AI_KEY"))
 
 
-def init_embeddings(impl) -> Embeddings:
-    if impl == "openai":
-        key = get_required_env("OPEN_AI_KEY")
-        return OpenAIEmbeddings(openai_api_key=key)
-    elif impl == "openai-azure":
+class AzureOpenAILLMContext(ContextMixin):
+    name = "openai-azure"
+
+    def __enter__(self) -> AzureChatOpenAI:
+        return AzureChatOpenAI(
+            azure_deployment=get_required_env("AZURE_OPEN_AI_CHAT_MODEL_DEPLOYMENT"),
+            openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
+            openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
+            openai_api_type="azure",
+            openai_api_version="2023-07-01-preview",
+        )
+
+
+class AzureOpenAIEmbeddingsContext(ContextMixin):
+    name = "openai-azure"
+
+    def __enter__(self) -> AzureOpenAIEmbeddings:
         model_and_deployment = get_required_env(
             "AZURE_OPEN_AI_EMBEDDINGS_MODEL_DEPLOYMENT"
         )
@@ -104,129 +152,127 @@ def init_embeddings(impl) -> Embeddings:
             openai_api_version="2023-05-15",
             chunk_size=1,
         )
-    elif impl == "vertex-ai":
-        return VertexAIEmbeddings(model_name="textembedding-gecko")
-    elif impl == "bedrock-titan":
-        return BedrockEmbeddings(
-            model_id="amazon.titan-embed-text-v1",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-    elif impl == "bedrock-cohere":
-        return BedrockEmbeddings(
-            model_id="cohere.embed-english-v3",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-    elif impl == "huggingface-hub":
-        return HuggingFaceInferenceAPIEmbeddings(
-            api_key=get_required_env("HUGGINGFACE_HUB_KEY"),
-            model_name="sentence-transformers/all-MiniLM-l6-v2",
-        )
-    else:
-        raise Exception("Unknown embedding implementation: " + impl)
 
 
-def close_embeddings(impl, embeddings: Embeddings):
-    pass
+class VertexLLMContext(ContextMixin):
+    name = "vertex-ai"
 
-
-def init_llm(impl) -> BaseLanguageModel:
-    if impl == "openai":
-        key = get_required_env("OPEN_AI_KEY")
-        return ChatOpenAI(
-            openai_api_key=key,
-            model="gpt-3.5-turbo-16k",
-            streaming=True,
-            temperature=0,
-        )
-    elif impl == "openai-azure":
-        model_and_deployment = get_required_env("AZURE_OPEN_AI_CHAT_MODEL_DEPLOYMENT")
-        azure_open_ai = AzureChatOpenAI(
-            azure_deployment=model_and_deployment,
-            openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
-            openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
-            openai_api_type="azure",
-            openai_api_version="2023-07-01-preview",
-        )
-        return azure_open_ai
-    elif impl == "vertex-ai":
+    def __enter__(self) -> ChatVertexAI:
         return ChatVertexAI()
-    elif impl == "bedrock-anthropic":
+
+
+class VertexEmbeddingsContext(ContextMixin):
+    name = "vertex-ai"
+
+    def __enter__(self) -> VertexAIEmbeddings:
+        return VertexAIEmbeddings(model_name="textembedding-gecko")
+
+
+class BedrockAnthropicLLMContext(ContextMixin):
+    name = "bedrock-anthropic"
+
+    def __enter__(self) -> BedrockChat:
         return BedrockChat(
             model_id="anthropic.claude-v2",
             region_name=get_required_env("BEDROCK_AWS_REGION"),
         )
-    elif impl == "bedrock-meta":
+
+
+class BedrockMetaLLMContext(ContextMixin):
+    name = "bedrock-meta"
+
+    def __enter__(self) -> BedrockChat:
         return BedrockChat(
             model_id="meta.llama2-13b-chat-v1",
             region_name=get_required_env("BEDROCK_AWS_REGION"),
         )
-    elif impl == "huggingface-hub":
+
+
+class BedrockTitanEmbeddingsContext(ContextMixin):
+    name = "bedrock-titan"
+
+    def __enter__(self) -> BedrockEmbeddings:
+        return BedrockEmbeddings(
+            model_id="amazon.titan-embed-text-v1",
+            region_name=get_required_env("BEDROCK_AWS_REGION"),
+        )
+
+
+class BedrockCohereEmbeddingsContext(ContextMixin):
+    name = "bedrock-cohere"
+
+    def __enter__(self) -> BedrockEmbeddings:
+        return BedrockEmbeddings(
+            model_id="cohere.embed-english-v3",
+            region_name=get_required_env("BEDROCK_AWS_REGION"),
+        )
+
+
+class HuggingFaceHubLLMContext(ContextMixin):
+    name = "huggingface-hub"
+
+    def __enter__(self) -> HuggingFaceHub:
         return HuggingFaceHub(
             repo_id="google/flan-t5-xxl",
             huggingfacehub_api_token=get_required_env("HUGGINGFACE_HUB_KEY"),
             model_kwargs={"temperature": 0.5, "max_length": 64},
         )
-    else:
-        raise Exception("Unknown llm implementation: " + impl)
 
 
-def close_llm(impl, llm: BaseLanguageModel):
-    pass
+class HuggingFaceHubEmbeddingsContext(ContextMixin):
+    name = "huggingface-hub"
+
+    def __enter__(self) -> HuggingFaceInferenceAPIEmbeddings:
+        return HuggingFaceInferenceAPIEmbeddings(
+            api_key=get_required_env("HUGGINGFACE_HUB_KEY"),
+            model_name="sentence-transformers/all-MiniLM-l6-v2",
+        )
 
 
 def test_openai_azure_astra_dev():
-    _run_test(
-        vector_db=VECTOR_ASTRADB_DEV, embedding="openai-azure", llm="openai-azure"
+    test_rag(
+        DevAstraDBVectorStoreContext,
+        AzureOpenAIEmbeddingsContext,
+        AzureOpenAILLMContext,
     )
 
 
 @pytest.mark.parametrize(
-    "vector_db",
+    "vector_store_ctx_cls",
     [
-        VECTOR_ASTRADB_PROD,
-        VECTOR_CASSANDRA,
+        ProdAstraDBVectorStoreContext,
+        CassandraVectorStoreContext,
     ],
 )
 @pytest.mark.parametrize(
-    "embedding,llm",
+    "embedding_ctx_cls,llm_ctx_cls",
     [
-        ("openai", "openai"),
-        ("openai-azure", "openai-azure"),
-        ("vertex-ai", "vertex-ai"),
-        ("bedrock-titan", "bedrock-anthropic"),
-        ("bedrock-cohere", "bedrock-meta"),
+        (OpenAIEmbeddingsContext, OpenAILLMContext),
+        (AzureOpenAIEmbeddingsContext, AzureOpenAILLMContext),
+        (VertexEmbeddingsContext, VertexLLMContext),
+        (BedrockTitanEmbeddingsContext, BedrockAnthropicLLMContext),
+        (BedrockCohereEmbeddingsContext, BedrockMetaLLMContext),
+        (HuggingFaceHubEmbeddingsContext, HuggingFaceHubLLMContext),
     ],
 )
-def test_rag(embedding: str, llm: str, vector_db: str):
-    _run_test(vector_db=vector_db, embedding=embedding, llm=llm)
-
-
-def test_huggingface_hub():
-    _run_test(
-        vector_db=VECTOR_ASTRADB_PROD,
-        embedding="huggingface-hub",
-        llm="huggingface-hub",
-    )
-
-
-def _run_test(vector_db: str, embedding: str, llm: str):
-    set_current_test_info("simple_rag", f"{llm},{embedding},{vector_db}")
-
-    embeddings_impl = init_embeddings(embedding)
-    vector_db_impl = init_vector_db(vector_db, embeddings_impl)
-    llm_impl = init_llm(llm)
-    try:
-        response = run_application(
-            question="When was released MyFakeProductForTesting for the first time ?",
-            vector_store=vector_db_impl,
-            llm=llm_impl,
-        )
-        print(f"Got response ${response}")
-        assert "2020" in response
-    finally:
-        if vector_db_impl:
-            close_vector_db(vector_db, vector_db_impl)
-        if embeddings_impl:
-            close_embeddings(embedding, embeddings_impl)
-        if llm_impl:
-            close_llm(llm, llm_impl)
+def test_rag(
+    vector_store_ctx_cls: Type[VectorStoreContext],
+    embedding_ctx_cls: Type[ContextMixin],
+    llm_ctx_cls: Type[ContextMixin],
+):
+    embedding_ctx = embedding_ctx_cls()
+    with embedding_ctx as embedding:
+        llm_ctx = llm_ctx_cls()
+        vector_store_ctx = vector_store_ctx_cls(embedding)
+        with vector_store_ctx as vector_store, llm_ctx as llm:
+            set_current_test_info(
+                "simple_rag",
+                f"{llm_ctx.name},{embedding_ctx.name},{vector_store_ctx.name}",
+            )
+            response = run_application(
+                question="When was released MyFakeProductForTesting for the first time ?",  # noqa: E501
+                vector_store=vector_store,
+                llm=llm,
+            )
+            print(f"Got response ${response}")
+            assert "2020" in response
