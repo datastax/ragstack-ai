7763ad2020db6f79753d4cfcbc4cfd8d5c18c89d
Christophe Bornet <cbornet@hotmail.com>
2023-12-10 19:43:14 +0100

    Refactor langchain tests using fixtures (#112)

--- ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_matrix.py
+++ ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_matrix.py
@@ -1,6 +1,3 @@
-from abc import ABC, abstractmethod
-from typing import Type
-
 import cassio
 import pytest
 from e2e_tests.conftest import (
@@ -22,257 +19,184 @@ from langchain.embeddings import (
 )
 from langchain.embeddings.azure_openai import AzureOpenAIEmbeddings
 from langchain.llms.huggingface_hub import HuggingFaceHub
-from langchain.schema.embeddings import Embeddings
-from langchain.schema.vectorstore import VectorStore
 from langchain.vectorstores import AstraDB, Cassandra
 
 
-class ContextMixin(ABC):
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        ...
+def astra_db(name, astra_ref: AstraRef):
+    delete_all_astra_collections(astra_ref)
 
-    @abstractmethod
-    def __enter__(self):
-        ...
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
-
-class VectorStoreContext(ContextMixin, ABC):
-    def __init__(self, embedding: Embeddings):
-        self.embedding = embedding
-
-    @abstractmethod
-    def __enter__(self) -> VectorStore:
-        ...
+    yield (
+        name,
+        lambda embedding: AstraDB(
+            collection_name=astra_ref.collection,
+            embedding=embedding,
+            token=astra_ref.token,
+            api_endpoint=astra_ref.api_endpoint,
+        ),
+    )
+    delete_astra_collection(astra_ref)
 
 
-class AstraCloudVectorStoreContext(VectorStoreContext, ABC):
-    def __init__(self, astra_ref: AstraRef, embedding: Embeddings):
-        super().__init__(embedding)
-        self.astra_ref = astra_ref
-
-    def __enter__(self):
-        delete_all_astra_collections(self.astra_ref)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        delete_astra_collection(self.astra_ref)
+@pytest.fixture
+def astra_db_prod():
+    yield from astra_db("astradb-prod", get_astra_prod_ref())
 
 
-class AstraDBVectorStoreContext(AstraCloudVectorStoreContext, ABC):
-    def __enter__(self) -> AstraDB:
-        super().__enter__()
-        return AstraDB(
-            collection_name=self.astra_ref.collection,
-            embedding=self.embedding,
-            token=self.astra_ref.token,
-            api_endpoint=self.astra_ref.api_endpoint,
-        )
+@pytest.fixture
+def astra_db_dev():
+    yield from astra_db("astradb-dev", get_astra_dev_ref())
 
 
-class DevAstraDBVectorStoreContext(AstraDBVectorStoreContext):
-    name = "astradb-dev"
-
-    def __init__(self, embedding: Embeddings):
-        super().__init__(get_astra_dev_ref(), embedding)
-
-
-class ProdAstraDBVectorStoreContext(AstraDBVectorStoreContext):
-    name = "astradb-prod"
-
-    def __init__(self, embedding: Embeddings):
-        super().__init__(get_astra_prod_ref(), embedding)
-
-
-class CassandraVectorStoreContext(AstraCloudVectorStoreContext):
-    name = "cassandra"
-
-    def __init__(self, embedding: Embeddings):
-        super().__init__(get_astra_prod_ref(), embedding)
-
-    def __enter__(self) -> Cassandra:
-        super().__enter__()
-        cassio.init(token=self.astra_ref.token, database_id=self.astra_ref.id)
-        return Cassandra(
-            embedding=self.embedding,
-            session=None,
-            keyspace="default_keyspace",
-            table_name=self.astra_ref.collection,
-        )
+@pytest.fixture
+def cassandra():
+    astra_ref = get_astra_prod_ref()
+    delete_all_astra_collections(astra_ref)
+    cassio.init(token=astra_ref.token, database_id=astra_ref.id)
+    yield "cassandra", lambda embedding: Cassandra(
+        embedding=embedding,
+        session=None,
+        keyspace="default_keyspace",
+        table_name=astra_ref.collection,
+    )
+    delete_astra_collection(astra_ref)
 
 
-class OpenAILLMContext(ContextMixin):
-    name = "openai"
-
-    def __enter__(self) -> ChatOpenAI:
-        return ChatOpenAI(
-            openai_api_key=get_required_env("OPEN_AI_KEY"),
-            model="gpt-3.5-turbo-16k",
-            streaming=True,
-            temperature=0,
-        )
-
-
-class OpenAIEmbeddingsContext(ContextMixin):
-    name = "openai"
-
-    def __enter__(self) -> OpenAIEmbeddings:
-        return OpenAIEmbeddings(openai_api_key=get_required_env("OPEN_AI_KEY"))
-
-
-class AzureOpenAILLMContext(ContextMixin):
-    name = "openai-azure"
-
-    def __enter__(self) -> AzureChatOpenAI:
-        return AzureChatOpenAI(
-            azure_deployment=get_required_env("AZURE_OPEN_AI_CHAT_MODEL_DEPLOYMENT"),
-            openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
-            openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
-            openai_api_type="azure",
-            openai_api_version="2023-07-01-preview",
-        )
-
-
-class AzureOpenAIEmbeddingsContext(ContextMixin):
-    name = "openai-azure"
-
-    def __enter__(self) -> AzureOpenAIEmbeddings:
-        model_and_deployment = get_required_env(
-            "AZURE_OPEN_AI_EMBEDDINGS_MODEL_DEPLOYMENT"
-        )
-        return AzureOpenAIEmbeddings(
-            model=model_and_deployment,
-            deployment=model_and_deployment,
-            openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
-            openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
-            openai_api_type="azure",
-            openai_api_version="2023-05-15",
-            chunk_size=1,
-        )
-
-
-class VertexLLMContext(ContextMixin):
-    name = "vertex-ai"
-
-    def __enter__(self) -> ChatVertexAI:
-        return ChatVertexAI()
-
-
-class VertexEmbeddingsContext(ContextMixin):
-    name = "vertex-ai"
-
-    def __enter__(self) -> VertexAIEmbeddings:
-        return VertexAIEmbeddings(model_name="textembedding-gecko")
-
-
-class BedrockAnthropicLLMContext(ContextMixin):
-    name = "bedrock-anthropic"
-
-    def __enter__(self) -> BedrockChat:
-        return BedrockChat(
-            model_id="anthropic.claude-v2",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-
-
-class BedrockMetaLLMContext(ContextMixin):
-    name = "bedrock-meta"
-
-    def __enter__(self) -> BedrockChat:
-        return BedrockChat(
-            model_id="meta.llama2-13b-chat-v1",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-
-
-class BedrockTitanEmbeddingsContext(ContextMixin):
-    name = "bedrock-titan"
-
-    def __enter__(self) -> BedrockEmbeddings:
-        return BedrockEmbeddings(
-            model_id="amazon.titan-embed-text-v1",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-
-
-class BedrockCohereEmbeddingsContext(ContextMixin):
-    name = "bedrock-cohere"
-
-    def __enter__(self) -> BedrockEmbeddings:
-        return BedrockEmbeddings(
-            model_id="cohere.embed-english-v3",
-            region_name=get_required_env("BEDROCK_AWS_REGION"),
-        )
-
-
-class HuggingFaceHubLLMContext(ContextMixin):
-    name = "huggingface-hub"
-
-    def __enter__(self) -> HuggingFaceHub:
-        return HuggingFaceHub(
-            repo_id="google/flan-t5-xxl",
-            huggingfacehub_api_token=get_required_env("HUGGINGFACE_HUB_KEY"),
-            model_kwargs={"temperature": 0.5, "max_length": 64},
-        )
-
-
-class HuggingFaceHubEmbeddingsContext(ContextMixin):
-    name = "huggingface-hub"
-
-    def __enter__(self) -> HuggingFaceInferenceAPIEmbeddings:
-        return HuggingFaceInferenceAPIEmbeddings(
-            api_key=get_required_env("HUGGINGFACE_HUB_KEY"),
-            model_name="sentence-transformers/all-MiniLM-l6-v2",
-        )
-
-
-def test_openai_azure_astra_dev():
-    test_rag(
-        DevAstraDBVectorStoreContext,
-        AzureOpenAIEmbeddingsContext,
-        AzureOpenAILLMContext,
+@pytest.fixture
+def openai_llm():
+    return "openai", ChatOpenAI(
+        openai_api_key=get_required_env("OPEN_AI_KEY"),
+        model="gpt-3.5-turbo-16k",
+        streaming=True,
+        temperature=0,
     )
 
 
+@pytest.fixture
+def openai_embedding():
+    return "openai", OpenAIEmbeddings(openai_api_key=get_required_env("OPEN_AI_KEY"))
+
+
+@pytest.fixture
+def azure_openai_llm():
+    return "azure-openai", AzureChatOpenAI(
+        azure_deployment=get_required_env("AZURE_OPEN_AI_CHAT_MODEL_DEPLOYMENT"),
+        openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
+        openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
+        openai_api_type="azure",
+        openai_api_version="2023-07-01-preview",
+    )
+
+
+@pytest.fixture
+def azure_openai_embedding():
+    model_and_deployment = get_required_env("AZURE_OPEN_AI_EMBEDDINGS_MODEL_DEPLOYMENT")
+    return "azure-openai", AzureOpenAIEmbeddings(
+        model=model_and_deployment,
+        deployment=model_and_deployment,
+        openai_api_key=get_required_env("AZURE_OPEN_AI_KEY"),
+        openai_api_base=get_required_env("AZURE_OPEN_AI_ENDPOINT"),
+        openai_api_type="azure",
+        openai_api_version="2023-05-15",
+        chunk_size=1,
+    )
+
+
+@pytest.fixture
+def vertex_llm():
+    return "vertex-ai", ChatVertexAI()
+
+
+@pytest.fixture
+def vertex_embedding():
+    return "vertex-ai", VertexAIEmbeddings(model_name="textembedding-gecko")
+
+
+@pytest.fixture
+def bedrock_anthropic_llm():
+    return "bedrock-anthropic", BedrockChat(
+        model_id="anthropic.claude-v2",
+        region_name=get_required_env("BEDROCK_AWS_REGION"),
+    )
+
+
+@pytest.fixture
+def bedrock_meta_llm():
+    return "bedrock-meta", BedrockChat(
+        model_id="meta.llama2-13b-chat-v1",
+        region_name=get_required_env("BEDROCK_AWS_REGION"),
+    )
+
+
+@pytest.fixture
+def bedrock_titan_embedding():
+    return "bedrock-titan", BedrockEmbeddings(
+        model_id="amazon.titan-embed-text-v1",
+        region_name=get_required_env("BEDROCK_AWS_REGION"),
+    )
+
+
+@pytest.fixture
+def bedrock_cohere_embedding():
+    return "bedrock-cohere", BedrockEmbeddings(
+        model_id="cohere.embed-english-v3",
+        region_name=get_required_env("BEDROCK_AWS_REGION"),
+    )
+
+
+@pytest.fixture
+def huggingface_hub_llm():
+    return "huggingface-hub", HuggingFaceHub(
+        repo_id="google/flan-t5-xxl",
+        huggingfacehub_api_token=get_required_env("HUGGINGFACE_HUB_KEY"),
+        model_kwargs={"temperature": 0.5, "max_length": 64},
+    )
+
+
+@pytest.fixture
+def huggingface_hub_embedding():
+    return "huggingface-hub", HuggingFaceInferenceAPIEmbeddings(
+        api_key=get_required_env("HUGGINGFACE_HUB_KEY"),
+        model_name="sentence-transformers/all-MiniLM-l6-v2",
+    )
+
+
+def test_openai_azure_astra_dev(astra_db_dev, azure_openai_embedding, azure_openai_llm):
+    _run_test(astra_db_dev, azure_openai_embedding, azure_openai_llm)
+
+
+@pytest.mark.parametrize("vector_store", ["astra_db_prod", "cassandra"])
 @pytest.mark.parametrize(
-    "vector_store_ctx_cls",
+    "embedding,llm",
     [
-        ProdAstraDBVectorStoreContext,
-        CassandraVectorStoreContext,
+        ("openai_embedding", "openai_llm"),
+        ("azure_openai_embedding", "azure_openai_llm"),
+        ("vertex_embedding", "vertex_llm"),
+        ("bedrock_titan_embedding", "bedrock_anthropic_llm"),
+        ("bedrock_cohere_embedding", "bedrock_meta_llm"),
+        ("huggingface_hub_embedding", "huggingface_hub_llm"),
     ],
 )
-@pytest.mark.parametrize(
-    "embedding_ctx_cls,llm_ctx_cls",
-    [
-        (OpenAIEmbeddingsContext, OpenAILLMContext),
-        (AzureOpenAIEmbeddingsContext, AzureOpenAILLMContext),
-        (VertexEmbeddingsContext, VertexLLMContext),
-        (BedrockTitanEmbeddingsContext, BedrockAnthropicLLMContext),
-        (BedrockCohereEmbeddingsContext, BedrockMetaLLMContext),
-        (HuggingFaceHubEmbeddingsContext, HuggingFaceHubLLMContext),
-    ],
-)
-def test_rag(
-    vector_store_ctx_cls: Type[VectorStoreContext],
-    embedding_ctx_cls: Type[ContextMixin],
-    llm_ctx_cls: Type[ContextMixin],
-):
-    embedding_ctx = embedding_ctx_cls()
-    with embedding_ctx as embedding:
-        llm_ctx = llm_ctx_cls()
-        vector_store_ctx = vector_store_ctx_cls(embedding)
-        with vector_store_ctx as vector_store, llm_ctx as llm:
-            set_current_test_info(
-                "simple_rag",
-                f"{llm_ctx.name},{embedding_ctx.name},{vector_store_ctx.name}",
-            )
-            response = run_application(
-                question="When was released MyFakeProductForTesting for the first time ?",  # noqa: E501
-                vector_store=vector_store,
-                llm=llm,
-            )
-            print(f"Got response ${response}")
-            assert "2020" in response
+def test_rag(vector_store, embedding, llm, request):
+    _run_test(
+        request.getfixturevalue(vector_store),
+        request.getfixturevalue(embedding),
+        request.getfixturevalue(llm),
+    )
+
+
+def _run_test(vector_store, embedding, llm):
+    embedding_name, embedding = embedding
+    vector_store_name, vector_store = vector_store
+    vector_store = vector_store(embedding)
+    llm_name, llm = llm
+    set_current_test_info(
+        "simple_rag",
+        f"{llm_name},{embedding_name},{vector_store_name}",
+    )
+    response = run_application(
+        question="When was released MyFakeProductForTesting for the first time ?",
+        vector_store=vector_store,
+        llm=llm,
+    )
+    print(f"Got response ${response}")
+    assert "2020" in response
