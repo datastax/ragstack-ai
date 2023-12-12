c66e7088f323037f7f11fce4a76d41863a387747
Nicolò Boschi <boschi1997@gmail.com>
2023-12-12 12:29:17 +0100

    Add license to package metadata

--- tests/unit-tests/test_ragstack.py
+++ tests/unit-tests/test_ragstack.py
@@ -1,4 +1,8 @@
 def test_import():
+    from llama_index.vector_stores import (
+        AstraDBVectorStore,
+        CassandraVectorStore,
+    )  # noqa
     from langchain.vectorstores import AstraDB  # noqa
     import langchain_core  # noqa
     import langsmith  # noqa
@@ -8,10 +12,8 @@ def test_import():
     import openai  # noqa
     import tiktoken  # noqa
 
-
 def test_meta():
     from importlib import metadata
-
     meta = metadata.metadata("ragstack-ai")
     assert meta["version"]
     assert meta["license"] == "BUSL-1.1"
