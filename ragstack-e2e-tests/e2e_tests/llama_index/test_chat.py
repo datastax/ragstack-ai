a6518e040cd1d209d12f054c7f8f4dbe9b581daa
Nicolò Boschi <boschi1997@gmail.com>
2024-01-08 11:49:19 +0100

    fix more

--- ragstack-e2e-tests/e2e_tests/llama_index/test_chat.py
+++ ragstack-e2e-tests/e2e_tests/llama_index/test_chat.py
@@ -15,14 +15,14 @@ def set_test_info(chat: str):
 #     return Gemini(model="models/gemini-pro")
 
 
-@pytest.mark.parametrize(
-    "chat",
-    [
-        # "gemini",
-    ],
-)
-def test_chat(chat, request):
-    set_test_info(chat)
-    chat_model = request.getfixturevalue(chat)
-    response = chat_model.complete("Hello! Where Archimede was born?")
-    assert "Syracuse" in response.content
+# @pytest.mark.parametrize(
+#     "chat",
+#     [
+#         # "gemini",
+#     ],
+# )
+# def test_chat(chat, request):
+#     set_test_info(chat)
+#     chat_model = request.getfixturevalue(chat)
+#     response = chat_model.complete("Hello! Where Archimede was born?")
+#     assert "Syracuse" in response.content
