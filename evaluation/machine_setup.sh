ce07b025cd3ab5c1027e8039197e729b3306e246
Jordan Frazier <jordan.frazier@datastax.com>
2024-01-17 14:11:31 -0800

    Add lc agent trulens notebook

--- evaluation/machine_setup.sh
+++ evaluation/machine_setup.sh
@@ -6,4 +6,4 @@ sudo yum install python311 python3-devel gcc postgresql-devel -y
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 pip3 install --upgrade pip
-pip3 install ragstack-ai ragas==0.0.19 python-dotenv pypdf pdfminer.six trulens_eval psycopg2 psutil IPython
+pip3 install python-dotenv pypdf pdfminer.six trulens_eval psycopg2 psutil IPython langchain llama-index
