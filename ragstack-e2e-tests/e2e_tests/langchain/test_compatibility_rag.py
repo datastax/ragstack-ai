9f8b6a340dc27ad3fcf2fb96bfde43b3db7d7425
Nicolò Boschi <boschi1997@gmail.com>
2024-01-23 09:01:58 +0100

    bump llama-index to 0.9.34 (#217)
    * bump llama-index to 0.9.34
    
    * Update test_compatibility_rag.py

--- ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_rag.py
+++ ragstack-e2e-tests/e2e_tests/langchain/test_compatibility_rag.py
@@ -306,7 +306,7 @@ def nvidia_embedding():
     get_required_env("NVIDIA_API_KEY")
     from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings
 
-    return NVIDIAEmbeddings(model="nvolve-40k")
+    return NVIDIAEmbeddings(model="playground_nvolveqa_40k")
 
 
 @pytest.fixture
@@ -314,7 +314,7 @@ def nvidia_mixtral_llm():
     get_required_env("NVIDIA_API_KEY")
     from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
-    return ChatNVIDIA(model="mixtral_8x7b")
+    return ChatNVIDIA(model="playground_mixtral_8x7b")
 
 
 @pytest.mark.parametrize(
