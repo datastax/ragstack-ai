1965aa416bb8fc7af68f6062b6914f0f90bc94be
Nicolò Boschi <boschi1997@gmail.com>
2024-01-25 10:20:12 +0100

    fix

--- ragstack-e2e-tests/e2e_tests/llama_index/test_compatibility_rag.py
+++ ragstack-e2e-tests/e2e_tests/llama_index/test_compatibility_rag.py
@@ -284,7 +284,7 @@ def gemini_pro_vision_llm():
     "embedding,llm",
     [
         # disable due to this bug: https://github.com/googleapis/python-aiplatform/issues/3227
-        #("vertex_gemini_multimodal_embedding", "vertex_gemini_pro_vision_llm"),
+        # ("vertex_gemini_multimodal_embedding", "vertex_gemini_pro_vision_llm"),
         ("vertex_gemini_multimodal_embedding", "gemini_pro_vision_llm"),
     ],
 )
