10d0dcd30ecfef6e09604ceab9df322ec2a3b758
Nicolò Boschi <boschi1997@gmail.com>
2024-01-31 20:55:42 +0100

    lint

--- ragstack-e2e-tests/e2e_tests/conftest.py
+++ ragstack-e2e-tests/e2e_tests/conftest.py
@@ -121,9 +121,9 @@ def pytest_runtest_makereport(item, call):
             test_outcome = f"(? {rep.outcome}))"
         result = " " + str(call.excinfo) if call.excinfo else ""
         report_line = f"{info} -> {test_outcome}{result} ({total_time} s)"
-        skip_report_line = (
-            rep.outcome == "skipped"
-            and (is_skipped_due_to_implementation_not_supported(result) or "unconditional skip" in result)
+        skip_report_line = rep.outcome == "skipped" and (
+            is_skipped_due_to_implementation_not_supported(result)
+            or "unconditional skip" in result
         )
         if not skip_report_line:
             logging.info("Test report line: " + report_line)
