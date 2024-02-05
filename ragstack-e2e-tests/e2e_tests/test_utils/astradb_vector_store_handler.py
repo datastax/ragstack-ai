056481d9061dc63887af56cb4e3361e7819bcafb
Nicolò Boschi <boschi1997@gmail.com>
2024-02-05 10:13:08 -0800

    tests: fix llamaindex astra vector store metaclass

--- ragstack-e2e-tests/e2e_tests/test_utils/astradb_vector_store_handler.py
+++ ragstack-e2e-tests/e2e_tests/test_utils/astradb_vector_store_handler.py
@@ -114,9 +114,16 @@ class EnhancedAstraDBLangChainVectorStore(EnhancedLangChainVectorStore, AstraDB)
         return docs
 
 
+def metaclass_resolver(*classes):
+    metaclass = tuple(set(type(cls) for cls in classes))
+    metaclass = metaclass[0] if len(metaclass)==1 \
+                else type("_".join(mcls.__name__ for mcls in metaclass), metaclass, {})   # class M_C
+    return metaclass("_".join(cls.__name__ for cls in classes), classes, {})
 class EnhancedAstraDBLlamaIndexVectorStore(
+metaclass_resolver(
     EnhancedLlamaIndexVectorStore, AstraDBVectorStore
-):
+)):
+
     def put_document(
         self, doc_id: str, document: str, metadata: dict, vector: List[float]
     ) -> None:
