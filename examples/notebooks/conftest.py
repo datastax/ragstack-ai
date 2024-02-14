4564ccbe03b0bf6a6208bde82ba9457a841668a8
Nicolò Boschi <boschi1997@gmail.com>
2024-02-14 15:30:45 +0100

    ci: enable notebook tests (#286)

--- examples/notebooks/conftest.py
+++ examples/notebooks/conftest.py
@@ -1,8 +1,20 @@
+import logging
 import os
+import time
 import uuid
 
-os.environ["ASTRA_DB_KEYSPACE"] = "default_keyspace"
-os.environ["ASTRA_DB_COLLECTION"] = f"astra_demo_{str(uuid.uuid4()).split('-')[0]}"
+import pytest
+from astrapy.db import AstraDB
+
+
+def get_required_env(name) -> str:
+    if name not in os.environ:
+        raise ValueError(f"Missing required environment variable: {name}")
+    value = os.environ[name]
+    if not value:
+        raise ValueError(f"Empty required environment variable: {name}")
+    return value
+
 
 # vertex-ai
 if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
@@ -10,3 +22,33 @@ if "GOOGLE_APPLICATION_CREDENTIALS" not in os.environ:
         f.write(os.getenv("GCLOUD_ACCOUNT_KEY_JSON", ""))
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/gcloud-account-key.json"
 
+client = AstraDB(
+    token=get_required_env("ASTRA_DB_TOKEN"),
+    api_endpoint=get_required_env("ASTRA_DB_ENDPOINT")
+)
+
+
+def try_delete_with_backoff(collection: str, sleep=1, max_tries=5):
+    try:
+        logging.info(f"deleting collection {collection}")
+        response = client.delete_collection(
+            collection
+        )
+        logging.info(f"delete collection {collection} response: {str(response)}")
+    except Exception as e:
+        max_tries -= 1
+        if max_tries < 0:
+            raise e
+
+        logging.warning(f"An exception occurred deleting collection {collection}: {e}")
+        time.sleep(sleep)
+        try_delete_with_backoff(collection, sleep * 2, max_tries)
+
+
+@pytest.fixture
+def cleanup_astra():
+    collections = client.get_collections().get("status").get("collections")
+    logging.info(f"Existing collections: {collections}")
+    for collection in collections:
+        try_delete_with_backoff(collection)
+    yield
