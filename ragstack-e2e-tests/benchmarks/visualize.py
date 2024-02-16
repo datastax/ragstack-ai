38cd9662327ca83da72a1d8cf1a06e9834cb1089
Jordan Frazier <jordan.frazier@datastax.com>
2024-02-15 19:09:43 -0800

    Add throughput plot to visualize

--- ragstack-e2e-tests/benchmarks/visualize.py
+++ ragstack-e2e-tests/benchmarks/visualize.py
@@ -3,13 +3,22 @@
 import argparse
 import base64
 import json
+import pandas as pd
 from io import BytesIO
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
 import numpy as np
 import os
 
 PERCENTILES = [50, 90, 95, 99]
+
+# Manually modify these to the desired values to avoid having to input them
+GPUS = 0
+NUM_CHUNKS = 0
+TITLE = None
+TITLE = "NeMo Embedding + AstraDB Store: Throughput and Inference Latency (A100)"
 
 
 def extract_values_from_result_file(file_path):
@@ -33,6 +42,122 @@ def scan_result_directory(directory_path, filter_by):
             file_path = os.path.join(directory_path, file_name)
             values.append(extract_values_from_result_file(file_path))
     return sorted(values, key=lambda v: v["values"]["p99"])
+
+
+def extract_threads(name):
+    """
+    Extracts the number of threads from the name, e.g.
+    embeddings_batch100_chunk512-nemo_microservice-8
+    """
+    return int(name.split("-")[-1])
+
+
+def render_throughput_plots(values):
+    """
+    Renders throughput and latency plots.
+
+    Note: Only nemo and openai items are supported
+    """
+    nemo_items = [item for item in values if "nemo" in item["name"]]
+    openai_items = [item for item in values if "openai" in item["name"]]
+
+    nemo_items_sorted = sorted(nemo_items, key=lambda x: extract_threads(x["name"]))
+    openai_items_sorted = sorted(openai_items, key=lambda x: extract_threads(x["name"]))
+    _render_throughput_plot(nemo_items_sorted, "NeMo")
+    _render_throughput_plot(openai_items_sorted, "OpenAI")
+
+
+def _render_throughput_plot(sorted_items, name):
+    print(f"Rendering plot for {name}")
+    threads = [extract_threads(item["name"]) for item in sorted_items]
+    chunk_sizes = [
+        int(item["name"].split("_")[2].split("-")[0].split("chunk")[1])
+        for item in sorted_items
+    ]
+    if len(set(chunk_sizes)) > 1:
+        raise ValueError("Throughput plots only work with a single chunk size")
+
+    batch_sizes = [
+        int(item["name"].split("_")[1].split("batch")[1]) for item in sorted_items
+    ]
+    if len(set(batch_sizes)) > 1:
+        raise ValueError("Throughput plots only work with a single batch size")
+
+    if GPUS == 0:
+        gpu = input("How many GPUs were used? ")
+        gpu = int(gpu)
+        gpus = [gpu for _ in range(len(threads))]
+    if NUM_CHUNKS == 0:
+        chunk = input("How many chunks were created? ")
+        chunk = int(chunk)
+        chunks = [chunk for _ in range(len(threads))]
+
+    p99_values = [item["values"]["p99"] for item in sorted_items]
+    throughput = [chunk / p for p in p99_values]
+    avg_inference = [1000 / t for t in throughput]
+
+    data = {
+        "Request Concurrency": threads,
+        "GPUs (A100)": gpus,
+        "Chunks": chunks,
+        "Approx. Chunk Size (B)": chunk_sizes,
+        "Benchmark Batch Size": batch_sizes,
+        "Throughput (infer/sec)": throughput,
+        "p99 Latency / Benchmark (s)": p99_values,
+        "Avg. Latency / Inference (ms)": avg_inference,
+    }
+    df = pd.DataFrame(data)
+
+    print(f"Saving markdown for {name} to benchmark_results-{name}.md")
+    markdown_string = df.to_markdown(index=False)
+    with open(f"benchmark_results-{name}.md", "w") as f:
+        f.write(markdown_string)
+
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+
+    color = "tab:red"
+    ax1.set_xlabel("Request Concurrency")
+    ax1.set_ylabel("Throughput (infer/sec)", color=color)
+    ax1.plot(
+        df["Request Concurrency"],
+        df["Throughput (infer/sec)"],
+        color=color,
+        marker="o",
+        label="Throughput",
+    )
+    ax1.tick_params(axis="y", labelcolor=color)
+    ax1.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    # Annotating each point with its value on ax1
+    for x, y in zip(df["Request Concurrency"], df["Throughput (infer/sec)"]):
+        ax1.text(x, y, f"{y:.2f}", color=color, ha="center", va="bottom")
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    color = "tab:blue"
+    ax2.set_ylabel("Avg. Latency / Inference (ms)", color=color)
+    ax2.plot(
+        df["Request Concurrency"],
+        df["Avg. Latency / Inference (ms)"],
+        color=color,
+        marker="o",
+        fillstyle="none",
+        label="Latency",
+    )
+    ax2.tick_params(axis="y", labelcolor=color)
+
+    # Annotating each point with its value on ax2
+    for x, y in zip(df["Request Concurrency"], df["Avg. Latency / Inference (ms)"]):
+        ax2.text(x, y, f"{y:.2f}", color=color, ha="center", va="bottom")
+
+    plt.subplots_adjust(top=0.92)
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    ax1.grid(True)
+
+    title = input("Enter Plot Title: ") if TITLE is None else TITLE
+    plt.title(title, pad=5)
+    file_name = f"benchmark_results-{name}.png"
+    print(f"Saving plot for {name} to {file_name}")
+    plt.savefig(file_name, bbox_inches="tight")
 
 
 def render_plot(values, export_to: str, show: bool = False):
@@ -171,25 +296,28 @@ def render_markdown(values, export_to: str, plot_src: str = "plot.png"):
     plot = f"<img src='{plot_src}' />"
 
     md = f"""# {title}
-
-{table}
-
-{plot}"""
+    {table}
+    {plot}"""
 
     with open(export_to, "w") as f:
         f.write(md)
 
 
-def draw_report(directory_path: str, format: str, filter_by: str):
+def draw_report(directory_path: str, format: str, filter_by: str, throughput: bool):
     values = scan_result_directory(directory_path, filter_by)
     is_all = format == "all"
     if is_all or format == "plot" or format == "plot_svg":
-        render_plot(
-            values,
-            export_to=os.path.join(directory_path, "plot.svg"),
-            show=format == "plot",
-        )
+        if throughput:
+            render_throughput_plots(values)
+        else:
+            render_plot(
+                values,
+                export_to=os.path.join(directory_path, "plot.svg"),
+                show=format == "plot",
+            )
     if is_all or format == "html":
+        if throughput:
+            raise NotImplementedError("Throughput mode is not supported for HTML")
         render_html(values, export_to=os.path.join(directory_path, "index.html"))
     if is_all or format == "markdown":
         render_plot(
@@ -203,6 +331,8 @@ def draw_report(directory_path: str, format: str, filter_by: str):
             plot_src="plot.svg",
         )
     if is_all or format == "table":
+        if throughput:
+            raise NotImplementedError("Throughput mode is not supported for table")
         render_table(values)
 
 
@@ -231,5 +361,13 @@ if __name__ == "__main__":
         default="",
         help="Filter results. e.g. to filter only openai, use: openai_",
     )
+
+    parser.add_argument(
+        "-t",
+        "--throughput",
+        action="store_true",
+        help="If this flag is set, more parameters to the markdown and plots will be required and added. Only `markdown` and `plot` is supported",
+    )
+
     args = parser.parse_args()
-    draw_report(args.reports_dir, args.format, args.filter)
+    draw_report(args.reports_dir, args.format, args.filter, args.throughput)
