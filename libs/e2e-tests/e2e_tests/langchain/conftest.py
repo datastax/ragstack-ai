29a0fad9f1a81ccd797b5628ae0b93a6c443825d
Nicolò Boschi <boschi1997@gmail.com>
2024-05-23 11:32:45 +0200

    feat: add langchain cassandra tool

---
+++ libs/e2e-tests/e2e_tests/langchain/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from e2e_tests.conftest import (
+    get_vector_store_handler,
+)
+
+from e2e_tests.test_utils.vector_store_handler import (
+    VectorStoreImplementation,
+)
+
+
+@pytest.fixture
+def astra_db():
+    handler = get_vector_store_handler(VectorStoreImplementation.ASTRADB)
+    context = handler.before_test()
+    yield context
+    handler.after_test()
+
+
+@pytest.fixture
+def cassandra():
+    handler = get_vector_store_handler(VectorStoreImplementation.CASSANDRA)
+    context = handler.before_test()
+    yield context
+    handler.after_test()
