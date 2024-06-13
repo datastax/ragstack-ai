b3c07167e4426e8e803f98227280c505b4c59ec8
Christophe Bornet <cbornet@hotmail.com>
2024-06-13 13:26:05 +0200

    Make LinkTag uninstantiable

--- libs/knowledge-store/ragstack_knowledge_store/link_tag.py
+++ libs/knowledge-store/ragstack_knowledge_store/link_tag.py
@@ -1,28 +1,38 @@
-from abc import ABC
 from dataclasses import dataclass
 from typing import Literal, Dict, Any, Set
 
 
 @dataclass(frozen=True)
-class LinkTag(ABC):
+class _LinkTag:
     kind: str
     tag: str
     direction: Literal["incoming", "outgoing", "bidir"]
 
 
 @dataclass(frozen=True)
+class LinkTag(_LinkTag):
+    def __init__(self, kind: str, tag: str, direction: str) -> None:
+        if self.__class__ == LinkTag:
+            raise TypeError("Abstract class LinkTag cannot be instantiated")
+        super().__init__(kind, tag, direction)
+
+
+@dataclass(frozen=True)
 class OutgoingLinkTag(LinkTag):
-    direction: Literal["outgoing"] = "outgoing"
+    def __init__(self, kind: str, tag: str) -> None:
+        super().__init__(kind=kind, tag=tag, direction="outgoing")
 
 
 @dataclass(frozen=True)
 class IncomingLinkTag(LinkTag):
-    direction: Literal["incoming"] = "incoming"
+    def __init__(self, kind: str, tag: str) -> None:
+        super().__init__(kind=kind, tag=tag, direction="incoming")
 
 
 @dataclass(frozen=True)
 class BidirLinkTag(LinkTag):
-    direction: Literal["bidir"] = "bidir"
+    def __init__(self, kind: str, tag: str) -> None:
+        super().__init__(kind=kind, tag=tag, direction="bidir")
 
 
 LINK_TAGS = "link_tags"
