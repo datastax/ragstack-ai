6716a14e0052442d78296b9523b76797b3ecb0dd
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2024-06-28 09:17:11 -0700

    use cassandra test store

--- libs/knowledge-store/tests/integration_tests/test_graph_store.py
+++ libs/knowledge-store/tests/integration_tests/test_graph_store.py
@@ -2,49 +2,26 @@ import secrets
 from typing import Iterator
 from langchain_openai import OpenAIEmbeddings
 import pytest
-from testcontainers.core.container import DockerContainer
-from testcontainers.core.waiting_utils import wait_for_logs
-from cassandra.cluster import Cluster
 from dotenv import load_dotenv
 
 from ragstack_knowledge_store.graph_store import GraphStore
+from ragstack_tests_utils import LocalCassandraTestStore
 
 load_dotenv()
 
 KEYSPACE = "default_keyspace"
 
 @pytest.fixture(scope="session")
-def cassandra_port() -> Iterator[int]:
-    # TODO: Allow running against local Cassandra and/or Astra using pytest option.
-    cassandra = DockerContainer("cassandra:5")
-    cassandra.with_exposed_ports(9042)
-    cassandra.with_env(
-        "JVM_OPTS",
-        "-Dcassandra.skip_wait_for_gossip_to_settle=0 -Dcassandra.initial_token=0",
-    )
-    cassandra.with_env("HEAP_NEWSIZE", "128M")
-    cassandra.with_env("MAX_HEAP_SIZE", "1024M")
-    cassandra.with_env("CASSANDRA_ENDPOINT_SNITCH", "GossipingPropertyFileSnitch")
-    cassandra.with_env("CASSANDRA_DC", "datacenter1")
-    cassandra.start()
-    wait_for_logs(cassandra, "Startup complete")
-    cassandra.get_wrapped_container().exec_run(
-        (
-            f"""cqlsh -e "CREATE KEYSPACE {KEYSPACE} WITH replication = """
-            '''{'class': 'SimpleStrategy', 'replication_factor': '1'};"'''
-        )
-    )
-    port = cassandra.get_exposed_port(9042)
-    print(f"Cassandra started. Port is {port}")
-    yield port
-    cassandra.stop()
+def cassandra() -> Iterator[LocalCassandraTestStore]:
+    store = LocalCassandraTestStore()
+    yield store
+
+    if store.docker_container:
+        store.docker_container.stop()
 
 @pytest.fixture
-def graph_store_factory(cassandra_port: int):
-    cluster = Cluster(
-        port=cassandra_port,
-    )
-    session = cluster.connect()
+def graph_store_factory(cassandra: LocalCassandraTestStore):
+    session = cassandra.create_cassandra_session()
     session.set_keyspace(KEYSPACE)
 
     embedding = OpenAIEmbeddings()
